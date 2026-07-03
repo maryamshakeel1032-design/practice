cb6b12fc2aa4b0bb4f630f3a8db63dcbb37bc0fd
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-03 22:57:24 +0100

    Printing butterfy * pattern

---
+++ code.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        if (i != n)
+        {
+            for (int j = 0; j < 2 * (n - i-1) ; j++)
+            {
+                cout << " ";
+            }
+            for (int j = 0; j <= i ; j++)
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+    // bottom
+    for (int i = 0; i <n; i++)
+    {
+        for (int j = 0; j <n-i; j++)
+        {
+            cout << "*";
+        }
+
+        if (i != 0)
+        {
+            for (int j =0 ; j <i*2  ; j++)
+            {
+                cout << " ";
+            }
+        }
+        for (int j = 0; j <n-i; j++)
+        {
+            cout << "*";
+        }
+        
+        cout << endl;
+    }
+    return 0;
+}
