f94a9013f931da64d142ab6e167eab110a94c2b9
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-13 00:02:17 +0100

    Printing with taking inputs.

--- array/loop.cpp
+++ array/loop.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main()
 {
     int marks[5] = {99, 101, 89, 98, 93};
+      for (int i = 0; i < sizeof(marks) / sizeof(int); i++)
+    {
+        cout <<"Enter marks:";
+        cin >> marks[i]  ;
+    }
     for (int i = 0; i < sizeof(marks) / sizeof(int); i++)
     {
         cout << marks[i] << endl;
     }
-    cout << sizeof(marks) << endl;
-    cout << sizeof(int) << endl;
-    cout << sizeof(marks) / sizeof(int) << endl;
+    
     return 0;
 }
