b3505ae7ab981c20f8f94903f5a1e1064f1721c0
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-13 12:23:03 +0100

    Fixing array size in a variable

--- array/smallest.cpp
+++ array/smallest.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-    int marks[5] = {99, 101, 89, 98, 93};
+    int marks[5] = {99, 101,-89, 98, 93};
     int size=5;
     int smallest=INT_FAST64_MAX;
-    for (int i =1; i<size; i++)
+    for (int i =0; i<size; i++)
     {
         if(marks[i]< smallest)
         {
