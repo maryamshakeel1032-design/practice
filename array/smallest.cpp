7f9db8991dfee84716e75fe79e67c28f33d33148
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-13 12:13:09 +0100

    Smallest fixing

--- array/smallest.cpp
+++ array/smallest.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main()
 {
     int marks[5] = {99, 101, 89, 98, 93};
+    int size=5;
     int smallest=INT_FAST64_MAX;
-    for (int i =1; i<5; i++)
+    for (int i =1; i<size; i++)
     {
         if(marks[i]< smallest)
         {
