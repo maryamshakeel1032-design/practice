7266903dc474520d3c9461275b88189ee3f7a190
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-13 22:54:37 +0100

    Largest index

--- array/index.cpp
+++ array/index.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 int main()
 {
-    int marks[5] = {99, 101, -89, 98, 93};
-    int size=5;
-    int largest=INT_FAST64_MIN;
-    for (int i =0; i<size; i++)
+    int marks[6] = {99, 101, -89, 98, 93,345};
+    int size=6;
+    int largest=marks[0];
+    int largestindex=0;
+    for (int i=1; i<size; i++)
     {
-        largest=max(marks[i], largest);
-        if(marks[i]<largest)
-        cout<<i<<endl;
+        if(marks[i]>largest)
+        {
+            largest=marks[i];
+             largestindex=i;
+        }
     }
+    cout<<"Largest num is:"<<largest<<endl;
+    cout<<"Largestindex will be:"<<largestindex<<endl;
     return 0;
 }
