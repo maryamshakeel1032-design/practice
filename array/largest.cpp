a62fa4f05dea2c6411a913c42a05c9942eef2165
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-13 13:05:08 +0100

    Using max(predifined function)

--- array/largest.cpp
+++ array/largest.cpp
@@ -3,15 +3,12 @@ using namespace std;
 
 int main()
 {
-    int marks[5] = {99, 101, 89, 98, 93};
+    int marks[5] = {99, 101, -89, 98, 93};
     int size=5;
     int largest=INT_FAST64_MAX;
     for (int i =0; i<size; i++)
     {
-        if(marks[i]> largest)
-        {
-            largest=marks[i];
-        }
+        largest=max(marks[i], largest);
     }
        cout << largest << endl; 
     return 0;
