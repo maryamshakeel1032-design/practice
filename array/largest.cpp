f1ec012bbbc3a073fecb6106c38eb35e6c82f4a1
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-13 12:27:42 +0100

    Finding Largest

---
+++ array/largest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks[5] = {99, 101, 89, 98, 93};
+    int size=5;
+    int largest=INT_FAST64_MAX;
+    for (int i =0; i<size; i++)
+    {
+        if(marks[i]> largest)
+        {
+            largest=marks[i];
+        }
+    }
+       cout << largest << endl; 
+    return 0;
+}
