e8b734760acc23c4d10eb1b76f414b3cf9d17f29
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-14 11:35:30 +0100

    With function of linearsearch

--- array/linearsearch.cpp
+++ array/linearsearch.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
+int linearsearch(int arr[],int size,int target)
+{
+for (int i=1; i<size; i++)
+    {
+        if(arr[i]==target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
 int main()
 {
-    int marks[7] = {4,5,7,8,1,2,5};
+    int arr[7] = {4,5,7,8,1,2,5};
     int size=7;
-    int index=0;
      int target=10;
-    for (int i=1; i<size; i++)
-    {
-        if(marks[i]==target)
-        {
-            index=i;
-        }
-    }
-    cout<<"Target will be:"<<index<<endl;
+    
+    cout<<"Target will be:"<<linearsearch(arr,size,target)<<endl;
     return 0;
 }
