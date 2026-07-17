0accf26fe4d5e675b64c8653e71ddf9b4720dc45
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-17 05:38:19 +0100

    Vector's  different functions

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -3,11 +3,22 @@
 using namespace std;
 
     int main()
-    {
-       vector<char> vec={'a','b','1','v','m','b'};
-       for(char val:vec)
+    { 
+
+       vector<int> vec;
+       cout<<vec.size()<<endl;
+       vec.push_back(25);
+       vec.push_back(35);
+       vec.push_back(45);
+       cout<<"Size After pushing_back:"<<vec.size()<<endl;
+       vec.pop_back();
+       cout<<"Size After pop_back:"<<vec.size()<<endl;
+       for(int val:vec)
        {
         cout<<val<<endl;
        }
+       cout<<vec.front()<<endl;
+       cout<<vec.back()<<endl;
+       cout<<vec.at(1)<<endl;
         return 0;
     }
