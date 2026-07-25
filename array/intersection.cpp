7d462302074611b8d049eaaa06ce238d595c2fab
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-07-25 20:53:11 +0100

    Intersection

---
+++ array/intersection.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void intersection(int arr[], int size, int com[],int sz)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i];
+        for (int j = 0; j < sz; j++)
+        {
+            com[j];
+            if (arr[i]== com[j])
+            {
+                cout << arr[i] << "  ";
+                break;
+            }
+        }
+    }
+}
+int main()
+{
+    int arr[] = {1, 3, 4, 6, 8};
+     int size = 5;
+    int com[] = {2, 3, 4, 6};
+    int sz = 4;
+    cout << "intersection num is:" ;
+     intersection(arr, size,com,sz);
+    return 0;
+}
