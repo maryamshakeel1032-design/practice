9addb41c8224379acb5e28be6a4391c1321686dd
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-08-05 16:06:42 +0100

    Buy and sell

--- buyandsell/buyandsell.cpp
+++ buyandsell/buyandsell.cpp
@@ -7,13 +7,13 @@ int main()
     int n = arr.size();
     int profit = 0;
     int mini = INT_FAST16_MAX;
-    int maxi = INT_FAST16_MIN;
+    int maxprofit = INT_FAST16_MIN;
     for (int i = 0; i < n; i++)
     {
         mini = min(mini, arr[i]);
         profit = arr[i] - mini;
-        maxi = max(maxi, profit);
+        maxprofit = max(maxprofit, profit);
     }
-    cout << maxi;
-    return maxi;
+    cout << maxprofit;
+    return maxprofit;
 }
