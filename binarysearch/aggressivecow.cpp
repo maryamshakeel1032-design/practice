86a937fbff30de77731e80c288b486cb8f7dff2e
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-08-13 10:40:04 +0100

    Aggressive cow

--- binarysearch/aggressivecow.cpp
+++ binarysearch/aggressivecow.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool ispossible(vector<int> arr, int N, int C, int minalloweddistance) // o(n)
 {
     int cow = 1, lastpos = arr[0];
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i < N; i++)
     {
         if (arr[i] - lastpos >= minalloweddistance)
         {
