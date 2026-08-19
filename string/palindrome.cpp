5c023924ace3e7a5f0c5a75b057b999866c162e3
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-08-19 21:33:33 +0100

    Palindrome

---
+++ string/palindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+bool ispalindrome(string str)
+{   int st=0,end=str.size()-1;
+   while(st<end)
+   {
+     if(str[st]==str[end])
+    {
+   return true;
+    }
+    else return false;
+   } 
+   return false;
+}
+int main()
+{   
+    string str="racecar"; 
+      cout<< ispalindrome(str)<<endl;
+      string str2="madam"; 
+      cout<< ispalindrome(str2)<<endl;
+      string str3="apnacollege"; 
+      cout<< ispalindrome(str3)<<endl;
+    return 0;
+}
