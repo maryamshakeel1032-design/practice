64ff39f22c5dac7a228498f519bf69a6dcc9999d
maryamshakeel1032-design <maryamshakeel1032@gmail.com>
2026-08-27 22:23:37 +0100

    optimize duplicate

---
+++ 2darray/3sum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+vector<vector<int>> twoSum(vector<int> &nums)   //tc:o(n*n*n *logn(unique tripet)) ,sc:(o(unique tripet))
+{
+    set<vector<int>> s;
+    vector<vector<int>> ans;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            for (int k = j + 1; k < nums.size(); k++)
+            {
+                if (nums[i] + nums[j] + nums[k] == 0)
+                {
+                    vector<int> trip = {nums[i], nums[j], nums[k]};
+                    sort(trip.begin(), trip.end());
+                    if (s.find(trip) == s.end())
+                    {
+                        s.insert(trip);
+                        ans.push_back(trip);
+                    }
+                }
+            }
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> ans = twoSum(nums);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << endl;
+        for (int j = 0; j < ans[0].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
